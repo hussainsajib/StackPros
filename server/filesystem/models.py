15630aa3bbae9ae86351d68e41064a5a7db1ecea
Hussain <m.hussainul.islam@gmail.com>
2022-06-05 14:49:02 -0400

    add: transformation method

--- server/filesystem/models.py
+++ server/filesystem/models.py
@@ -4,9 +4,14 @@ from django.db import models
 class FileTypes(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class FileStructure(models.Model):
     name = models.CharField(max_length=100)
-    type = models.ForeignKey("filesystem.FileTypes", on_delete=models.CASCADE)
-    child = models.ForeignKey("self", on_delete=models.DO_NOTHING, null=True, related_name="childs_file")
-    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, related_name="parents_file")
+    file_type = models.ForeignKey("filesystem.FileTypes", on_delete=models.CASCADE)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, related_name="parents_file", default=None)
+
+    def __str__(self):
+        return f'{self.name} (type: {self.file_type}), parent: {self.parent}'
--- server/server/transform.py
+++ server/server/transform.py
@@ -6,16 +6,37 @@ from filesystem.models import FileTypes, FileStructure
 def transform_file_structure():
     with open("./structure.json", "r") as file:
         data = json.loads(file.read())
-        name, cat, value = "", data["type"], data["children"]
-        children = [[name, cat, value]]
-        while children:
+        name, cat, value = "root", data["type"], data["children"]
+        root_node, created = file_object_factory(name, cat)
+        stack = [[name, cat, value]]
+        while stack:
             if isinstance(value, str):
                 file_type, _ = FileTypes.objects.get_or_create(name=value)
             else:
-                for name, child_attributes in value.items():
-                    for cat, child in child_attributes.items():
-                        if child_attributes[cat] == "file":
-                            print(name)
-                        children.append([name, cat, child])
-            name, cat, value = children.pop()
+                for child_name, child_attributes in value.items():
+                    for key, val in child_attributes.items():
+                        if key == "type" and val == "file":
+                            f, _ = file_object_factory(child_name, val)
+                            f.parent = root_node
+                            f.save()
+                        elif key == "type" and val == "dir":
+                            d, _ = file_object_factory(child_name, val)
+                            d.parent = root_node
+                            d.save()
+                            root_node = d
+                        if key == "children" and val:
+                            stack.append([child_name, child_attributes["type"], child_attributes["children"]])
+            name, cat, value = stack.pop()
 
+
+def file_object_factory(name, category):
+    file_type = FileTypes.objects.get(name=category)
+    file, created = FileStructure.objects.get_or_create(
+        name=name,
+        file_type=file_type,
+        defaults={
+            "child": None,
+            "parent": None
+        }
+    )
+    return file, created
